fd8fdd0f92820ed1640d2e28207464537dfb1c79
Sanket Koli <kolisanket15@gmail.com>
2023-06-11 17:33:30 -0700

    next config for env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config()
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  },
   reactStrictMode: true,
 }
 
